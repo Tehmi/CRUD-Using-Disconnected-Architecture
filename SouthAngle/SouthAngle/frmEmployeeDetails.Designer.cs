59e8ad698dd6a276791149175ff381c34538c150
Tehmi <Tehmina_yaseen@hotmail.co.uk>
2016-02-06 16:45:13 +0500

    South Angle
    This a Simple example of basic CRUD Operations using Disconnected
    Architecture

---
+++ SouthAngle/SouthAngle/frmEmployeeDetails.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace SouthAngle
+{
+    partial class frmEmployeeDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtDesignation = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtDOJ = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtContact = new System.Windows.Forms.TextBox();
+            this.lblRecord = new System.Windows.Forms.Label();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name: ";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(124, 13);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(308, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtDesignation
+            // 
+            this.txtDesignation.Location = new System.Drawing.Point(124, 60);
+            this.txtDesignation.Name = "txtDesignation";
+            this.txtDesignation.Size = new System.Drawing.Size(308, 20);
+            this.txtDesignation.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Designation:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Date Of Joining:";
+            // 
+            // dtDOJ
+            // 
+            this.dtDOJ.CustomFormat = "MM/dd/yyyy";
+            this.dtDOJ.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtDOJ.Location = new System.Drawing.Point(124, 103);
+            this.dtDOJ.Name = "dtDOJ";
+            this.dtDOJ.Size = new System.Drawing.Size(308, 20);
+            this.dtDOJ.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Gender:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(124, 142);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(308, 33);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(185, 6);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(59, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Female";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 7);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(48, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Male";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 203);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Contact Number: ";
+            // 
+            // txtContact
+            // 
+            this.txtContact.Location = new System.Drawing.Point(124, 200);
+            this.txtContact.Name = "txtContact";
+            this.txtContact.Size = new System.Drawing.Size(308, 20);
+            this.txtContact.TabIndex = 9;
+            // 
+            // lblRecord
+            // 
+            this.lblRecord.AutoSize = true;
+            this.lblRecord.Location = new System.Drawing.Point(459, 250);
+            this.lblRecord.Name = "lblRecord";
+            this.lblRecord.Size = new System.Drawing.Size(48, 13);
+            this.lblRecord.TabIndex = 10;
+            this.lblRecord.Text = "Record: ";
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(181, 273);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(51, 33);
+            this.btnLast.TabIndex = 11;
+            this.btnLast.Text = "<<";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(250, 273);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(51, 33);
+            this.btnPrevious.TabIndex = 12;
+            this.btnPrevious.Text = "<";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(318, 273);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(50, 33);
+            this.btnNext.TabIndex = 13;
+            this.btnNext.Text = ">";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Location = new System.Drawing.Point(379, 273);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(53, 33);
+            this.btnFirst.TabIndex = 14;
+            this.btnFirst.Text = ">>";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(142, 331);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 15;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(241, 330);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 16;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(341, 330);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 17;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(447, 330);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 18;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmEmployeeDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 383);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnFirst);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.lblRecord);
+            this.Controls.Add(this.txtContact);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dtDOJ);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDesignation);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Name = "frmEmployeeDetails";
+            this.Text = "Employee Details";
+            this.Load += new System.EventHandler(this.frmEmployeeDetails_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtDesignation;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtDOJ;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtContact;
+        private System.Windows.Forms.Label lblRecord;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ SouthAngle/SouthAngle/frmEmployeeDetails.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SouthAngle
+{
+    public partial class frmEmployeeDetails : Form
+    {
+        SqlConnection con;
+        SqlDataAdapter da;
+        DataSet ds;
+        BindingSource bs;
+
+        public frmEmployeeDetails()
+        {
+            InitializeComponent();
+            con = new SqlConnection(@"Data Source=FACULTY03\APTECH;Initial Catalog=SouthAngle;Integrated Security=True");
+            da = new SqlDataAdapter();
+            SqlCommandBuilder cb = new SqlCommandBuilder(da);
+            ds = new DataSet();
+            bs = new BindingSource();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmEmployeeDetails_Load(object sender, EventArgs e)
+        {
+            if (con.State == ConnectionState.Open)
+                con.Close();
+            ds.Clear();
+            con.Open();
+            da.SelectCommand = new SqlCommand("select * from Employee",con);
+            da.Fill(ds,"Employee");
+            bs.Clear();
+            foreach(DataRow drow in ds.Tables["Employee"].Rows)
+            {
+                bs.Add(drow);
+            }
+            DisplayRecord();
+            EnableTextControls(false);
+            EnableDisableControls("First");
+            btnSave.Enabled = false;
+            btnCancel.Enabled = false;
+            btnAdd.Enabled = true;
+            bs.PositionChanged += new EventHandler(bs_PositionChanged);
+            lblRecord.Text = "Record: "+ (bs.Position+1) + " of "+ bs.Count;
+
+        }
+
+        private void EnableDisableControls(string cursorPosition)
+        {
+            if (cursorPosition == "First")
+            {
+                btnFirst.Enabled = false;
+                btnPrevious.Enabled = false;
+                btnNext.Enabled = true;
+                btnPrevious.Enabled = true;
+            }
+            else if (cursorPosition == "Last")
+            {
+                btnFirst.Enabled = true;
+                btnPrevious.Enabled = true;
+                btnNext.Enabled = false;
+                btnLast.Enabled = false;
+            }
+            else if (cursorPosition == "InBetween")
+            {
+                btnFirst.Enabled = true;
+                btnPrevious.Enabled = true;
+                btnNext.Enabled = true;
+                btnLast.Enabled = true;
+            }
+            else if (cursorPosition == "AfterLast")
+            {
+                btnFirst.Enabled = false;
+                btnPrevious.Enabled = false;
+                btnNext.Enabled = false;
+                btnLast.Enabled = false;
+            }
+        }
+
+        private void EnableTextControls(bool enable)
+        {
+            txtName.Enabled = enable;
+            txtDesignation.Enabled = enable;
+            radioButton1.Enabled = enable;
+            radioButton2.Enabled = enable;
+            dtDOJ.Enabled = enable;
+            txtContact.Enabled = enable;
+            
+        }
+
+        private void DisplayRecord()
+        {
+            DataRow drow = (DataRow)bs.Current;
+            txtName.Text = drow["Name"].ToString();
+             txtDesignation.Text = drow["Designation"].ToString();
+             dtDOJ.Text = drow["DOJ"].ToString();
+             if (drow["Gender"].ToString().Equals("Male"))
+                 radioButton1.Checked = true;
+             else
+                 radioButton2.Checked = true;
+             txtContact.Text = drow["ContactNumber"].ToString();       
+        
+        }
+
+        private void bs_PositionChanged(object sender, EventArgs e)
+        {
+            lblRecord.Text = "Record: " + (bs.Position + 1) + " of " + bs.Count; ;
+        }
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            bs.MoveFirst();
+            DisplayRecord();
+            EnableDisableControls("First");
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            bs.MoveLast();
+            DisplayRecord();
+            EnableDisableControls("Last");
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+              if(bs.Position == bs.Count -1)
+              {
+                  MessageBox.Show("LAst Record. ","SouthAngle",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                  EnableDisableControls("Last");
+              }
+              else
+              {
+                  bs.MoveNext();
+                  DisplayRecord();
+                  EnableDisableControls("InBetween");
+              }
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            if (bs.Position == 0)
+            {
+                MessageBox.Show("First record. ", "SouthAngle",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                EnableDisableControls("First");
+            }
+            else
+            {
+                bs.MovePrevious();
+                DisplayRecord();
+                EnableDisableControls("InBetween");
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            txtName.Text = "";
+            txtDesignation.Text = "";
+            dtDOJ.Text = "";
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            txtContact.Text = "";
+            btnAdd.Enabled = false;
+            btnSave.Enabled = true;
+            btnCancel.Enabled = true;
+            EnableTextControls(true);
+            EnableDisableControls("AfterLast");
+            lblRecord.Text = "New Record";
+            txtName.Focus();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text == "")
+            {
+                MessageBox.Show("Enter the name. ","SothAngle",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                txtName.Focus();
+                return;
+            }
+
+            if (txtDesignation.Text == "")
+            {
+                MessageBox.Show("Enter the Designation. ", "SothAngle", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtDesignation.Focus();
+                return;
+            }
+
+            if (dtDOJ.Text == "")
+            {
+                MessageBox.Show("Enter the Date ", "SothAngle", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                dtDOJ.Focus();
+                return;
+            }
+           
+            if (txtContact.Text== "")
+            {
+                MessageBox.Show("Enter the Contact ", "SothAngle", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtContact.Focus();
+                return;
+            }
+            DataRow drowemp = ds.Tables["Employee"].NewRow();
+            drowemp["Name"] = txtName.Text;
+            drowemp["Designation"] = txtDesignation.Text;
+            drowemp["DOJ"] = dtDOJ.Text;
+            if(radioButton1.Checked)
+            drowemp["Gender"] = radioButton1.Text;
+            else
+                drowemp["Gender"] =radioButton2.Text;
+            drowemp["ContactNumber"] = txtContact.Text;
+            ds.Tables["Employee"].Rows.Add(drowemp);
+            da.Update(ds,"Employee");
+            MessageBox.Show("Record has been successfully added.","SouthAngle",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            frmEmployeeDetails_Load(null,null);
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            frmEmployeeDetails_Load(null,null);
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
